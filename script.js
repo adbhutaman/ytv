e33652caa39f1da874e931191a0c7806a3741f81
AMAN SINGH <121595073+adbhutaman@users.noreply.github.com>
2023-12-16 10:54:20 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const apiKey = 'AIzaSyDhfy7TDw-UsjWXGXgbuDrHbaujCpEs1FM';
+const script = document.createElement('script');
+script.src = `https://www.youtube.com/iframe_api?api_key=${apiKey}`;
+document.head.appendChild(script);
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        playerVars: {
+            'autoplay': 0,
+            'controls': 1,
+            'showinfo': 0,
+            'rel': 0,
+            'fs': 1,
+            'modestbranding': 1,
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Player is ready
+}
+
+function onPlayerStateChange(event) {
+    // Handle player state changes if needed
+}
+
+function loadVideo() {
+    const videoId = document.getElementById('videoId').value;
+    if (videoId) {
+        player.loadVideoById(videoId);
+    } else {
+        alert('Please enter a valid YouTube Video ID.');
+    }
+}
